a41e2ae4663b37493cf648b3af593eb7babc7314
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-08-04 15:08:28 -0500

    Clase 3: Uso de while y do while

---
+++ js/Clase3/While/script.js
@@ -0,0 +1,16 @@
+//          Uso de While
+number = 0
+while (number < 10) {
+    console.log(number)
+    number++
+}
+
+
+
+//      Do while
+
+// let password = `EDteam`
+// let pass
+// do {
+//     pass = prompt(`Ingresa la contraseña: `)    //Tiene que igualarse a la contraseña
+// } while (pass !== password)
