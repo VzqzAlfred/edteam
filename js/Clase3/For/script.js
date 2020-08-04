d5da96f9343f6eac47e4b977dfd9ce3911ca8758
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-08-04 14:12:32 -0500

    Uso de For

---
+++ js/Clase3/For/script.js
@@ -0,0 +1,22 @@
+//          Uso del FOR
+
+//          Número par e impar 
+/*
+    for(let i=0; i<101; i++){
+        if(i%2 === 0 ){
+            console.log(`El número ${i} es par`)
+        }else{
+            console.log(`El número ${i} es impar`)
+        }
+    }
+*/
+//      Múltiplos de 3
+
+    for(let i=1; i<=100; i++){
+        if(i%7 === 0){
+            console.log(`El número ${i} es múltiplo de 3`)
+        }
+        else {
+            console.log(`El número ${i} no es múltiplo de 3`)
+        }
+    }
