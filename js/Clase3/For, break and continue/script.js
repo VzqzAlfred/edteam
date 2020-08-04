cc86693160f974626b05e5dbb8b85d2df3fce238
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-08-04 14:44:31 -0500

    Clase 3: For, break and Continue

--- js/Clase3/For, break and continue/script.js
+++ js/Clase3/For, break and continue/script.js
@@ -10,13 +10,31 @@
         }
     }
 */
-//      Múltiplos de 3
+//      Múltiplos de 7
 
     for(let i=1; i<=100; i++){
         if(i%7 === 0){
-            console.log(`El número ${i} es múltiplo de 3`)
+            console.log(`El número ${i} es múltiplo de 7`)
         }
         else {
-            console.log(`El número ${i} no es múltiplo de 3`)
+            console.log(`El número ${i} no es múltiplo de 7`)
         }
     }
+
+
+
+
+
+    //  Uso del break y continue
+
+    // 5 primeros multiplos de 7
+let n=0    
+for (let j=1; j<=100; j++){
+    if(j%7 === 0){
+        console.log(j)
+        n++
+    }
+    if(n>=5) break
+}
+
+
