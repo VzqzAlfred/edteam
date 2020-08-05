278078816cff9059646feacad42c23f7e9f0ce32
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-08-04 21:47:58 -0500

    Clase 4: Funciones

---
+++ js/Clase4/script.js
@@ -0,0 +1,19 @@
+/*
+    Es un trozo de código re utilizable, en el que hay un conjunto de instrucciones
+
+    1. Como funciona    
+        °   input => funcion => output
+        °   input => funcion1 => output1 => funcion2 => output2 => funcion3 => output3 =>...
+
+    2. input => argumentos              // Los datos de entrada se llamana argumentos
+    3. output => valor de retorno       // Los datos de salida se llama valor de retorno
+
+    4. Como ejecutar una función
+        °   nombreFuncion(argumento1, argumento2,...)
+        °   nombreFuncion()             // En caso de que no haya argumentos
+
+    5. Una función puede ser método de un objeto    
+            // objeto.nombreFuncion(argumentos)
+                °   'Hola mundo'.slice(3)
+
+*/
