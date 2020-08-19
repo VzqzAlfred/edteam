dc4b8f8832becbc93beb4963e0994d5673b99ea6
VzqzAlfred <alfred.vzqz96@outlook.com>
2020-08-18 22:04:37 -0500

    Clase 5: Array

--- js/Clase5/script.js
+++ js/Clase5/script.js
@@ -63,8 +63,48 @@ console.log(arr);
 arr.splice(2,1,'New Zeland')
 console.log(arr);
 
+/*      MÉTODOS PARA ORDENAR ELEMENTOS DE UN ARRAY
+    .reverse()                  Pone al réves un texto
+    .sort(callback)             Ordena un array (Solo sirve en letras)
+*/
+            //split separa cada letra
+console.log('Anita no está aquí'.split('').reverse().join(''));
+
+const reverseText = string => string.split('').reverse().join('');     //join une la palabra
+reverseText('Anita no es de él  ')
+
+/*  Ejemplo de sort() */
+let arr2 = ['a,e,r,d,s,z,p,ñ']
+console.log(arr2.sort())
+console.log(arr2.sort().reverse())
 
 
 
+/*  Sin en cambio se puede ordenar de las siguiente manera*/
+let cadena = [20,1,10,100,1000,50,45,92,165,300,8165,9530,1592]
+console.log(cadena);
+
+console.log(cadena.sort((a,b) => a - b))        //La funcion ordena de par en par cada número
+
+/*      CONVERTIR ARRAYS A STRINGS 
+    .join()
+        Convierte el arrayt a string
+    .concat()
+        recibe como paràmetro un array o una lista
+*/
+// Unimos la cadena con comas
+console.log(cadena.join(','))
 
 
+let cadena1 = [1,5,21,56,474]
+console.log(cadena.concat(cadena1))     //ó concat(15,654,41)
+
+/*      Métodos para encontrar elementos d eun array
+    indexOf(value)
+    .find(callback)
+    .findIndex(callback)
+*/
+
+console.log(cadena.indexOf(50)) // Busca la ubicación del 50
+console.log(cadena.find( cadena => cadena > 100))   // Te devuelve el primer numero mayor que 100 
+console.log(cadena.findIndex( cadena => cadena > 100))   // Lo mismo que el anterior, pero te devuelve la posición
