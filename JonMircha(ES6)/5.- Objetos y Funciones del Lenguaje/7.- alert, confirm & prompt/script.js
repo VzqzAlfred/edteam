afe821e70384489f0f9510ceb24c95ec79377593
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-02-08 21:21:25 -0600

    alert, confirm & prompt

---
+++ JonMircha(ES6)/5.- Objetos y Funciones del Lenguaje/7.- alert, confirm & prompt/script.js
@@ -0,0 +1,10 @@
+            /* alert, confirm & prompt */
+
+console.log(window);
+//alert("Hola, esto es un alerta");
+//confirm("Esto es una confirmación.");
+//prompt("Ingresa algo:")
+
+let alerta = alert("Hola, esto es un alerta")
+    confirmacion = confirm("Esto es una confirmación.")
+    aviso = prompt("Ingresa algo:");
