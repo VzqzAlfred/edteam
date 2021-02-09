ed222dce980c287137dfef051b17cc5c546b5707
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-02-08 20:48:19 -0600

    Objeto Math

---
+++ JonMircha(ES6)/5.- Objetos y Funciones del Lenguaje/3 Objeto Math/script.js
@@ -0,0 +1,15 @@
+            /* Objeto Math */
+
+console.log(Math);
+console.log(Math.abs(7.8));
+console.log(Math.random()*100); // Número aleatorio *10^-3
+console.log(Math.ceil(7.4));    // Redondea a uno mayor
+console.log(Math.floor(7.7));   // Redondea ignorando el decimal
+console.log(Math.round(7.2));   // Redondea al más cercano inmediato
+console.log(Math.round(7.5));     
+console.log(Math.sqrt(81)); 
+console.log(Math.pow(2, 5));    // Base y exponente
+console.log(Math.sign(-4.8));   // Indica si es 1 ó -1 (positivo o negativo)
+
+// Numeros aleatorios del 1 al 100
+console.log(Math.round(Math.random() * 100));   
