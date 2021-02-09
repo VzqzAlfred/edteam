1085b70c0e1db5a4e72af969cc0be1680e81cde3
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-02-08 21:05:06 -0600

    Operador de Cortocircuito

---
+++ JonMircha(ES6)/5.- Objetos y Funciones del Lenguaje/6.- Operador de Cortocircuito/script.js
@@ -0,0 +1,28 @@
+            /* Operador de Cortocircuito. */
+
+/*
+    Cortocircuito OR: Cuando el valor de la izquierda en la expresión siempre pueda validar a true, es el valor que se encargará por defecto.
+    
+    Cortocircuito AND: Cuando el valor de la izquierda en la expresión siempre pueda validar a false, es el valor que se encargará por defecto.
+*/
+
+const saludar = nombre => {
+    nombre = nombre || "Desconocido";
+    console.log(`Hola ${nombre}`); 
+}
+
+saludar();
+saludar("Alfred");
+
+console.log("cadena" || "valor de la derecha");
+console.log("0" || "valor de la derecha");
+console.log(false || "valor de la derecha");
+console.log(null || "valor de la derecha");
+console.log(-0 || "valor de la derecha");
+
+console.log("\n");
+
+console.log("cadena" && "valor de la derecha");
+console.log(false && "valor de la derecha");
+console.log(null && "valor de la derecha");
+console.log(-0 && "valor de la derecha");
