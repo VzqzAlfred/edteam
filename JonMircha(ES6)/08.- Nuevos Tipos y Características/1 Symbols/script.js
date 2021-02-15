2619b947505f76adfbeefd1d3f9a6c21cc105877
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-02-15 12:38:22 -0600

    Symbols

---
+++ JonMircha(ES6)/08.- Nuevos Tipos y Características/1 Symbols/script.js
@@ -0,0 +1,52 @@
+            /* Symbols */
+
+// Sumbol(): Es un tipo de dato primitivo, se mantienen privados y se manejo interno. Nos permiten crear identificadores unicos 
+// * Symbol() va sin el *new*
+
+let id = Symbol("id"), 
+    id2 = Symbol("id");
+
+console.log(id === id2);
+console.log(id,id2);
+console.log(typeof(id), typeof(id2));
+
+const NOMBRE = Symbol("name");
+const SALUDAR = Symbol("saludar");
+
+// Los corchete hacen referencia a que es un tipo symbol
+const persona = {
+    [NOMBRE]: "Alfred symbol"
+}
+console.log(persona);
+
+
+
+persona.NOMBRE = "Alfredo";
+console.log("\n",persona);
+
+console.log(persona.NOMBRE);
+console.log(persona[NOMBRE]);
+
+
+
+//Agregando otro simbolo al objeto
+persona[SALUDAR] = function(){
+    console.log("Hola");
+}
+
+
+
+// Accede a las propiedades pero no a los symbols porque son privados.
+console.log(persona);
+persona[SALUDAR]();
+
+for (let propiedad in persona) {
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+}
+
+
+
+
+// Accediendo a los symbols, listandolos
+console.log(Object.getOwnPropertySymbols(persona));
