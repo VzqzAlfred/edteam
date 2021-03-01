ab9a307114f23e58c1f20e70d9b0b3db6a6f8fe5
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-03-01 15:46:01 -0600

    ScrollSpy con Intersection Observer

---
+++ JonMircha(ES6)/12.- Ejercicios DOM/19 ScrollSpy con Intersection Observer/sections/deteccion_webcam.js
@@ -0,0 +1,21 @@
+
+const d = document,
+      n = navigator;
+
+export default function webCam(id){
+    const $video = d.getElementById(id);
+    //console.log(n.mediaDevices.getUserMedia);
+    if(n.mediaDevices.getUserMedia){
+        n.mediaDevices
+        .getUserMedia({video: true, audio: false})
+        .then(stream => {
+            console.log(stream);
+            $video.srcObject = stream;
+            $video.play();
+        })
+        .catch(err => { 
+            $video.insertAdjacentHTML("beforebegin", `<p><mark>Sucedio el siguiente error: ${err}</mark></p>`);
+            //console.log(`Sucedio el siguiente error: ${err}`)
+        });
+    }
+}
