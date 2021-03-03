0ef0adc877e589ff9c46f0f1b04a12949aa28a76
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-03-03 12:04:12 -0600

    Introducción a AJAX

---
+++ JonMircha(ES6)/13.- AJAX: Asynchronous JavaScript And XML/1 Introducción a AJAX/script.js
@@ -0,0 +1,48 @@
+        /* Introducción a AJAX */
+/*
+    AJAX: Asynchronous JavaScript And XML
+*/
+
+
+
+//? Métodos Nativos.
+/*
+!    - ActiveXObject (IES e inferiores)(Ya obsoleto era para Internet Explloreer)
+
+?    - XMLHttpRequest: Objecto que describe AJAX
+
+?    - API Fetch: Es una forma moderna de hacer AJAX
+*/
+
+
+
+
+
+//? Liberrías Externas
+/*
+        - JQuery.ajax()
+        
+        - Axios
+
+        - etc
+*/
+
+
+
+
+
+// Una peticion de AJAX va a tener 4 estados:
+/*
+    Estado	                    Valor
+ READY_STATE_UNINITIALIZED	      0
+ READY_STATE_LOADING	          1
+ READY_STATE_LOADED	              2
+ READY_STATE_INTERACTIVE	      3
+ READY_STATE_COMPLETE	          4
+*/
+
+
+
+
+
+//TODO: https://api.github.com/users/VzqzAlfred
