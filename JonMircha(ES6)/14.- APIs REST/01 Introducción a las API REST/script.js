c7e6411e3464e3c715315b4f1ee3e58ca71a844f
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-03-03 23:38:02 -0600

    Introducción API-REST

---
+++ JonMircha(ES6)/14.- APIs REST/01 Introducción a las API REST/script.js
@@ -0,0 +1,15 @@
+            /* Introducción API-REST */
+
+/*
+    API: Application Programming Interface; conjunto de rutinas que nos provee acceso a ciertas funcionalidades de un software en específico.  
+*/
+/*
+    API Nativas: Todo lo que JS trae integrado. 
+    
+    API Externas: Enlaces externos (github, twitter y etc).
+*/
+/*
+    El término REST (Representational State Transfer) o Transferencia de Estado Representacional.
+
+    Un servicio REST es un conjunto de restricciones con las que podemos crear un estilo de arquitectura de software, la cual podremos usar para crear aplicaciones web respetando el protocolo HTTP.
+*/
