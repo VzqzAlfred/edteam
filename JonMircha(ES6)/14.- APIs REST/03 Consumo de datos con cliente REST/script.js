20b5a94769562c4f115a41503dff732f8787e402
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-03-04 14:53:03 -0600

    Consumo de datos con cliente REST

---
+++ JonMircha(ES6)/14.- APIs REST/03 Consumo de datos con cliente REST/script.js
@@ -0,0 +1,10 @@
+        /* Consumo de datos con cliente REST */
+
+/*
+Se uso INSOMIA
+TODO: CRUD
+    GET (READ)      -     CONSULTAR       
+    POST (CREATE)   -     CREAR 
+    PUT (UPDATE)    -     ACTUALIZAR o MODIFICAR
+    DELETE (DELETE) -     BORRAR
+*/
