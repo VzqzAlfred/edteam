2d64e406ab807278c66f9d5fb97834e4fb25c275
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-03-07 13:46:49 -0600

    Uploader Drag & Drop con AJAX y PHP

---
+++ JonMircha(ES6)/15.- Ejercicios de AJAX y APIs/05 Uploader AJAX con Drag & Drop (Usando PHP)/assets/uploader.php
@@ -0,0 +1,30 @@
+<?php
+    //echo "Hola, respuesta desde el servidor";
+    //var_dump($_FILES);
+
+    if(isset($_FILES["file"])){
+        $name = $_FILES["file"]["name"];
+        $file = $_FILES["file"]["tmp-name"];
+        $error = $_FILES["file"]["error"];
+        $destination = "./files/$name";
+
+        $uplodad = move_uploaded_file($file, $destination);
+
+
+        if($uplodad){
+            $res = array(
+                "err" => false,
+                "status" => http_response_code(200),
+                "statusText" => "Archivo $name subido con éxito.",
+                "files" => $_FILES["file"]
+            ); 
+        }else {
+            $res = array(
+                "err" => true,
+                "status" => http_response_code(400),
+                "statusText" => "Error al subir el archivo $name.",
+                "files" => $_FILES["file"]
+            );    
+        }
+        echo json_encode($res);
+    }
