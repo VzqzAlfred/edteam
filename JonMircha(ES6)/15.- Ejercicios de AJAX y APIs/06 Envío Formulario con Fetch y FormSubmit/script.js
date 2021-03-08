9ed29ec5e782240439dd3accc1ef10a4b0668c7b
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-03-08 13:51:49 -0600

    Envío Formulario con Fetch y FormSubmit

---
+++ JonMircha(ES6)/15.- Ejercicios de AJAX y APIs/06 Envío Formulario con Fetch y FormSubmit/script.js
@@ -0,0 +1,74 @@
+const d = document;
+
+function contactForm() {
+  const $form = d.querySelector(".contact-form "),
+    $inputs = d.querySelectorAll(".contact-form [required]");
+
+  //console.log($inputs);
+  $inputs.forEach((input) => {
+    const $span = d.createElement("span");
+    $span.id = input.name;
+    $span.textContent = input.title;
+    $span.classList.add("contact-form-error", "none");
+    input.insertAdjacentElement("afterend", $span);
+  });
+
+  d.addEventListener("keyup", (e) => {
+    if (e.target.matches(".contact-form [required]")) {
+      let $input = e.target,
+        pattern = $input.pattern || $input.dataset.pattern;
+      //console.log($input, pattern);
+
+      if (pattern && $input.value !== "") {
+        //console.log("El input tiene patrón");
+        let regex = new RegExp(pattern);
+
+        return !regex.exec($input.value)
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      }
+
+      if (!pattern) {
+        //console.log("El input NO tiene patrón");
+        return $input.value === ""
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      }
+    }
+  });
+
+  d.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //alert(`Enviando formulario...`);
+
+    const $loader = d.querySelector(".contact-form-loader"),
+      $response = d.querySelector(".contact-form-response");
+
+    $loader.classList.remove("none");
+
+    fetch("https://formsubmit.co/ajax/alfred.vzqz239@gmail.com", {
+        method: "POST",
+        body: new FormData(e.target)
+    })
+        .then(res => res.ok ? res.json() : Promise.reject(res))
+        .then(json => {
+            console.log(json);
+            $loader.classList.add("none");
+            $response.classList.remove("none");
+            $response.innerHTML = `<p>${json.message}</p>`
+            $form.reset();
+        })
+        .catch(err => {
+            console.log(err);
+            let message = err.statuText || "Ocurrió un error, intentalo nuevamente";
+            $response.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
+        })
+        .finally(() => setTimeout(() => {
+            $response.classList.add("none");
+            $response.innerHTML = "";
+        }, 3000));
+  });
+}
+
+
+d.addEventListener("DOMContentLoaded", contactForm)
